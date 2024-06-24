3d18e6fcb98a8a96131303a9a9340830b2416452
acatarip <acatarip@unjg.edu.pe>
2024-06-24 09:00:55 -0500

    Se agrego el bucle do-while. Se arreglo el caso 2.

--- funciones1.cpp
+++ funciones1.cpp
@@ -11,33 +11,37 @@ int main (){
     int op;
     float monto;
     system("cls");
-    cout<<"Cajero automatico"<<endl;
-    cout<<"1. Depositar"<<endl;
-    cout<<"2. Retirar"<<endl;
-    cout<<"3. Mostrar saldo"<<endl;
-    cout<<"0. Salir"<<endl;
-    cout<<"Elige una opcion: ";cin>>op;
-    switch (op){
-        case 1:
-            cout<<"Ingrese monto a depositar: "; cin>>monto;
-            depositar(monto);
-        break;
-        case 2:
-            cout<<"Ingrese monto a retirar: "; cin>>monto;
-        break;
-        case 3:
-            mostrarSaldo();
-        break;
-        case 0:
-            cout<<"Saliendo..."<<endl;
-            system("Pause");
-        break;
-        default:
-            cout<<"Opcion no valida. Saliendo..."<<endl;
-            system("Pause");
-        break;
-    }
 
+    do {
+       cout<<"\n\tCajero automatico"<<endl;
+        cout<<"1. Depositar"<<endl;
+        cout<<"2. Retirar"<<endl;
+        cout<<"3. Mostrar saldo"<<endl;
+        cout<<"0. Salir"<<endl;
+        cout<<"Elige una opcion: ";cin>>op;
+        switch (op){
+            case 1:
+                cout<<"Ingrese monto a depositar: "; cin>>monto;
+                depositar(monto);
+            break;
+            case 2:
+                cout<<"Ingrese monto a retirar: "; cin>>monto;
+                retirar(monto);
+            break;
+            case 3:
+                mostrarSaldo();
+            break;
+            case 0:
+                cout<<"Saliendo..."<<endl;
+                system("Pause");
+            break;
+            default:
+                cout<<"Opcion no valida. Saliendo..."<<endl;
+                system("Pause");
+            break;
+        }
+
+    }while (op!=0);
     return 0;
 }
 
