ebb0f64230357a636c3e3b631b6457ca5f00a368
acatarip <acatarip@unjg.edu.pe>
2024-06-24 11:13:10 -0500

    Se agrego 3 funciones de redondeo.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -6,10 +6,14 @@ int main (){
     system ("cls");
     double nn;
     cout<<"Ingrese un numero: "; cin>>nn;
+    cout<<endl;
 
     cout<<"Valor absoluto de "<<nn<<" es "<<fabs(nn)<<endl;
     cout<<"Raiz cuadrada de "<<nn<<" es "<<sqrt(nn)<<endl;
     cout<<"Potencia (n^2) de "<<nn<<" es "<<pow(nn, 2)<<endl;
+    cout<<"Redondeo al entero mas cercano de "<<nn<<" es "<<round(nn)<<endl;
+    cout<<"Redondeo hacia arriba de "<<nn<<" es "<<ceil(nn)<<endl;
+    cout<<"Redondeo hacia abajo de "<<nn<<" es "<<floor(nn)<<endl;
 
 
     return 0;
