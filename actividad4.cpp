6e95f41a24476246a66a32754f0626b1e163f8cb
acatarip <acatarip@unjbg.edu.pe>
2024-07-01 04:01:41 -0500

    Se añadió el archivo actividad4.cpp

---
+++ actividad4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void raices(float, float, float);
+
+int main(){
+	float a, b, c;
+	cout<<"Para la ecuacion cuadratica ax^2 + bx + c = 0"<<endl;
+	cout<<"Ingrese el valor de a: ";cin>>a;
+	if(a==0){
+		cout<<"El valor de a no puede ser cero.";
+	}
+	else{
+		cout<<"Ingrese el valor de b: ";cin>>b;
+		cout<<"Ingrese el valor de c: ";cin>>c;
+		cout<<endl<<"Para la ecuacion: "<<a<<"x^2 + "<<b<<"x + "<<c<<" = 0"<<endl;
+		raices(a, b, c);
+	}
+	
+	return 0;
+} 
+void raices(float a, float b, float c){
+	float d, x1, x2;
+	d=pow(b,2) - (4*a*c);
+	
+	if(d>0){	
+		x1=(-b + sqrt(d)) / (2*a);
+		x2=(-b - sqrt(d)) / (2*a);
+		cout<<"Las raices son reales: "<<x1<<" y "<<x2<<"."<<endl;	
+	}
+}
