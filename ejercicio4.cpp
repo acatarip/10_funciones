3251d1ab39ba519a10edde5b493285783811928e
acatarip <acatarip@unjbg.edu.pe>
2024-06-30 19:23:42 -0500

    Se añadio estructura while para adivinar el numero en ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -7,6 +7,20 @@ int main (){
 	srand (time(0));
 	int nMagico=rand() % 100 + 1;
 	cout<<nMagico;    //esta linea sirve para probar el programa, luego se eliminara
-
+	int intento;
+	
+	cout<<"Adivina el numero magico (entre 1 y 100): ";
+	while (true){
+		cin>>intento;
+		if (intento < nMagico){
+			cout<<"Muy bajo. Intenta de nuevo: ";
+		} else if (intento > nMagico){
+			cout<<"Muy alto. Intenta de nuevo: ";
+		} else {
+			cout<< "Asertaste. El numero magico es "<<nMagico<<endl;
+			break;
+		}
+		
+	}
 	return 0;
 }
